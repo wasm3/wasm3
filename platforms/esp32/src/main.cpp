8eae567f51af7301f4244a149855faadd2220c09
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2019-10-25 01:33:42 +0300

    Update ESP32 platform

--- platforms/esp32/src/main.cpp
+++ platforms/esp32/src/main.cpp
@@ -8,7 +8,7 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
-#define FATAL(msg, ...) { printf("Fatal: " msg "\n", __VA_ARGS__); return; }
+#define FATAL(msg, ...) { printf("Fatal: " msg "\n", ##__VA_ARGS__); return; }
 
 #include "m3/m3.hpp"
 
@@ -21,63 +21,43 @@ void run_wasm()
     u8* wasm = (u8*)fib_wasm;
     u32 fsize = fib_wasm_len-1;
 
-    printf("WASM: %d bytes\n", fsize);
+    printf("Loading WebAssembly...\n");
 
     IM3Module module;
     result = m3_ParseModule (& module, wasm, fsize);
     if (result) FATAL("m3_ParseModule: %s", result);
 
-    IM3Runtime env = m3_NewRuntime (8192);
+    IM3Runtime env = m3_NewRuntime (1024);
+    if (!env) FATAL("m3_NewRuntime");
 
     result = m3_LoadModule (env, module);
     if (result) FATAL("m3_LoadModule: %s", result);
 
-    //m3_LinkFunction (module, "_m3TestOut", "v(iFi)", (void *) m3TestOut);
-    //m3_LinkFunction (module, "_m3StdOut", "v(*)", (void *) m3Output);
-    //m3_LinkFunction (module, "_m3Export", "v(*i)", (void *) m3Export);
-    //m3_LinkFunction (module, "_m3Out_f64", "v(F)", (void *) m3Out_f64);
-    //m3_LinkFunction (module, "_m3Out_i32", "v(i)", (void *) m3Out_i32);
-    //m3_LinkFunction (module, "_TestReturn", "F(i)", (void *) TestReturn);
-
-    //m3_LinkFunction (module, "abortStackOverflow",	"v(i)", (void *) m3_abort);
-
-    //result = m3_LinkCStd (module);
-    //if (result) FATAL("m3_LinkCStd: %s", result);
-
-    m3_PrintRuntimeInfo (env);
-
     IM3Function f;
-    result = m3_FindFunction (&f, env, "__post_instantiate");
-    if (! result)
-    {
-      result = m3_Call (f);
-      if (result) FATAL("__post_instantiate: %s", result);
-    }
-
     result = m3_FindFunction (&f, env, "_fib");
     if (result) FATAL("m3_FindFunction: %s", result);
 
-    clock_t start = clock();
+    printf("Running...\n");
 
-    const char* i_argv[2] = { "32", NULL };
+    const char* i_argv[2] = { "24", NULL };
     result = m3_CallWithArgs (f, 1, i_argv);
 
-    clock_t end = clock();
-
-    uint32_t elapsed_time = (end - start)*1000 / CLOCKS_PER_SEC ;
-    printf("Elapsed: %d ms\n", elapsed_time);
-
-    m3_PrintProfilerInfo ();
-
-    if (result) FATAL("Call: %s", result);
+    if (result) FATAL("m3_CallWithArgs: %s", result);
 }
 
 void wasm_task(void*)
 {
-  run_wasm();
-  for(;;) {
-    vTaskDelay(0xFFFF);
-  }
+    printf("\nwasm3 on ESP32, build " __DATE__ " " __TIME__ "\n");
+
+    clock_t start = clock();
+    run_wasm();
+    clock_t end = clock();
+
+    printf("Elapsed: %d ms\n", (end - start)*1000 / CLOCKS_PER_SEC);
+
+    for(;;) {
+        vTaskDelay(0xFFFF);
+    }
 }
 
 extern "C"
@@ -98,11 +78,7 @@ void app_main()
 
     xTaskCreate(&wasm_task, "wasm_m3", 32768, NULL, 5, NULL);
 
-    for (;;) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    for(;;) {
+        vTaskDelay(0xFFFF);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
-
 }
