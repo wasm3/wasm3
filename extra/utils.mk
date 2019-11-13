771e1535e7364ab49c7f77515673d792c9525665
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2019-11-13 02:41:03 +0200

    Add preprocess

--- extra/utils.mk
+++ extra/utils.mk
@@ -7,3 +7,11 @@ format:
 	find ./source -type f -iname '*.[cpp\|c\|h]' -exec bash -c 'expand -t 4 "$$0" | sponge "$$0"' {} \;
 	# Remove trailing whitespaces
 	find ./source -type f -iname '*.[cpp\|c\|h]' -exec sed -i 's/ *$$//' {} \;
+
+preprocess:
+	cp ./source/m3_exec.h    ./source/m3_exec.h.bak
+	cp ./source/m3_compile.c ./source/m3_compile.c.bak
+	awk 'BEGIN {RS=""}{gsub(/\\\n/,"\\\n__NL__ ")}1' ./source/m3_exec.h | sponge ./source/m3_exec.h
+	cpp -P ./source/m3_compile.c | sponge ./source/m3_compile.c
+	awk '{gsub(/__NL__/,"\n")}1' ./source/m3_compile.c | sponge ./source/m3_compile.c
+	mv ./source/m3_exec.h.bak ./source/m3_exec.h
