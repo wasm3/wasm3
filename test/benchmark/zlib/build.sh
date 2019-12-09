e24f7f2b6b0f39774dbb72e8dcf653517ebd4d0d
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2019-12-09 18:35:42 +0200

    Remove old zlib test

--- test/benchmark/zlib/build.sh
+++
@@ -1,3 +0,0 @@
-source ../../../emsdk/emsdk_env.sh
-emcc -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s SIDE_MODULE=1 -O1 --profiling-funcs -I ./zlib/ -I ../../source/ ./zlib/compress.c ./zlib/deflate.c ./zlib/crc32.c ./zlib/adler32.c \
-	./zlib/trees.c ./zlib/zutil.c  test.zlib.c -o zlib.wasm
