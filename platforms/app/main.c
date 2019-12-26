d8c696f095054f45f88fbaf17e5fcbde1f4037d3
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2019-12-26 02:02:26 +0200

    Cleanup

--- platforms/app/main.c
+++ platforms/app/main.c
@@ -24,11 +24,17 @@ M3Result repl_load  (IM3Runtime runtime, const char* fn)
     fsize = ftell(f);
     fseek (f, 0, SEEK_SET);
 
-    if (fsize > 10*1024*1024) {
+    if (fsize < 8) {
+        return "file is too small";
+    } else if (fsize > 10*1024*1024) {
         return "file too big";
     }
 
     wasm = (u8*) malloc(fsize);
+    if (!wasm) {
+        return "cannot allocate memory for wasm binary";
+    }
+
     fread (wasm, 1, fsize, f);
     fclose (f);
 
