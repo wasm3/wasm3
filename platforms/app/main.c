2a8b85a80149f425a6e4a2cad057bbc5ab1a7133
Joel Martin <github@martintribe.org>
2020-01-17 15:55:32 -0600

    wasi/main: return exit code on wasi proc_exit call
    When running a file (non-REPL mode), if the proc_exit call is called,
    then terminate with the exit code. If in REPL mode then print
    a message with the exit code to indicate the wasi call was made and
    continue.

--- platforms/app/main.c
+++ platforms/app/main.c
@@ -273,6 +273,9 @@ int  main  (int i_argc, const char* i_argv[])
             } else {
                 result = repl_call(runtime, argFunc, i_argc, i_argv);
             }
+            if (result == m3Err_trapExit) {
+                return runtime->exit_code;
+            }
             if (result) FATAL("repl_call: %s", result);
         }
     }
@@ -314,6 +317,10 @@ int  main  (int i_argc, const char* i_argv[])
             M3ErrorInfo info;
             m3_GetErrorInfo (runtime, &info);
             fprintf (stderr, " (%s)\n", info.message);
+            if (result == m3Err_trapExit) {
+                // warn that exit was called
+                fprintf(stderr, M3_ARCH "-wasi: exit(%d)\n", runtime->exit_code);
+            }
         }
     }
 
