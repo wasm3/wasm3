b8e2486d0db3bcdf429a6ce140fa978ef0ad7192
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2020-01-20 05:27:15 +0200

    Fix string literal

--- source/m3.h
+++ source/m3.h
@@ -1,6 +1,6 @@
 
 #if defined(_MSC_VER)
-#pragma message(m3.h is deprecated, please use wasm3.h)
+#pragma message("m3.h is deprecated, please use wasm3.h")
 #else
 #warning "m3.h is deprecated, please use wasm3.h"
 #endif
