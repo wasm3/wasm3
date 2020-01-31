dee53d1e1eb915424bcd037f70e38d9ee3e8e080
MaxGraey <maxgraey@gmail.com>
2020-01-31 17:03:46 +0200

    Implement nontrapping-float-to-int-conversions

--- source/m3_math_utils.h
+++ source/m3_math_utils.h
@@ -197,6 +197,17 @@ u64 rotr64(u64 n, unsigned c) {
     }                                                       \
     RES = (TYPE)A;
 
+#define OP_TRUNC_SAT(RES, A, TYPE, RMIN, RMAX)              \
+    if (UNLIKELY(isnan(A))) {                               \
+        return 0;                                           \
+    }                                                       \
+    if (UNLIKELY(A <= RMIN)) {                              \
+        return RMIN;                                        \
+    }                                                       \
+    if (UNLIKELY(A >= RMAX)) {                              \
+        return RMAX;                                        \
+    }                                                       \
+    RES = (TYPE)A;
 
 #define OP_I32_TRUNC_F32(RES, A)    OP_TRUNC(RES, A, i32, -2147483904.0f, 2147483648.0f)
 #define OP_U32_TRUNC_F32(RES, A)    OP_TRUNC(RES, A, u32,          -1.0f, 4294967296.0f)
@@ -207,6 +218,16 @@ u64 rotr64(u64 n, unsigned c) {
 #define OP_U64_TRUNC_F32(RES, A)    OP_TRUNC(RES, A, u64,                   -1.0f, 18446744073709551616.0f)
 #define OP_I64_TRUNC_F64(RES, A)    OP_TRUNC(RES, A, i64, -9223372036854777856.0 ,  9223372036854775808.0 )
 #define OP_U64_TRUNC_F64(RES, A)    OP_TRUNC(RES, A, u64,                   -1.0 , 18446744073709551616.0 )
+
+#define OP_I32_TRUNC_SAT_F32(RES, A)    OP_TRUNC_SAT(RES, A, i32, -2147483904.0f, 2147483648.0f)
+#define OP_U32_TRUNC_SAT_F32(RES, A)    OP_TRUNC_SAT(RES, A, u32,          -1.0f, 4294967296.0f)
+#define OP_I32_TRUNC_SAT_F64(RES, A)    OP_TRUNC_SAT(RES, A, i32, -2147483649.0 , 2147483648.0 )
+#define OP_U32_TRUNC_SAT_F64(RES, A)    OP_TRUNC_SAT(RES, A, u32,          -1.0 , 4294967296.0 )
+
+#define OP_I64_TRUNC_SAT_F32(RES, A)    OP_TRUNC_SAT(RES, A, i64, -9223373136366403584.0f,  9223372036854775808.0f)
+#define OP_U64_TRUNC_SAT_F32(RES, A)    OP_TRUNC_SAT(RES, A, u64,                   -1.0f, 18446744073709551616.0f)
+#define OP_I64_TRUNC_SAT_F64(RES, A)    OP_TRUNC_SAT(RES, A, i64, -9223372036854777856.0 ,  9223372036854775808.0 )
+#define OP_U64_TRUNC_SAT_F64(RES, A)    OP_TRUNC_SAT(RES, A, u64,                   -1.0 , 18446744073709551616.0 )
 
 /*
  * Min, Max
