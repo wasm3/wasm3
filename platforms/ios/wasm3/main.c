e64c5ec9f02f203176e624cb682f54b24f41c2eb
MaxGraey <maxgraey@gmail.com>
2020-02-07 19:56:04 +0200

    update

--- platforms/ios/wasm3/main.c
+++ platforms/ios/wasm3/main.c
@@ -11,6 +11,7 @@
 #include <inttypes.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/utsname.h>
 
 #include "wasm3.h"
 #include "m3_env.h"
@@ -18,56 +19,62 @@
 
 #include "fib32.wasm.h"
 
+#define FIB_ARG_VALUE  "40"
 #define FATAL(msg, ...) { printf("Fatal: " msg "\n", ##__VA_ARGS__); return; }
 
 void run_wasm()
 {
     M3Result result = m3Err_none;
-    
+
     uint8_t* wasm = (uint8_t*)fib32_wasm;
-    uint32_t fsize = fib32_wasm_len-1;
-    
+    uint32_t fsize = fib32_wasm_len - 1;
+
     printf("Loading WebAssembly...\n");
-    
+
     IM3Environment env = m3_NewEnvironment ();
     if (!env) FATAL("m3_NewEnvironment failed");
-    
+
     IM3Runtime runtime = m3_NewRuntime (env, 8192, NULL);
     if (!runtime) FATAL("m3_NewRuntime failed");
-    
+
     IM3Module module;
     result = m3_ParseModule (env, &module, wasm, fsize);
     if (result) FATAL("m3_ParseModule: %s", result);
-    
+
     result = m3_LoadModule (runtime, module);
     if (result) FATAL("m3_LoadModule: %s", result);
-    
+
     IM3Function f;
     result = m3_FindFunction (&f, runtime, "fib");
     if (result) FATAL("m3_FindFunction: %s", result);
+
+    printf("Running fib(%s)...\n", FIB_ARG_VALUE);
+
+    const char* i_argv[2] = { FIB_ARG_VALUE, NULL };
     
-    printf("Running...\n");
+    // warmup
+    m3_CallWithArgs (f, 1, i_argv);
     
-    const char* i_argv[2] = { "40", NULL };
+    clock_t start = clock();
     result = m3_CallWithArgs (f, 1, i_argv);
-    
+    clock_t end = clock();
+
     if (result) FATAL("m3_CallWithArgs: %s", result);
-    
-    long value = *(uint64_t*)(runtime->stack);
-    printf("Result: %ld\n", value);
+    printf("Elapsed: %ld ms\n", (end - start) * 1000 / CLOCKS_PER_SEC);
+
+    // uint64_t value = *(uint64_t*)(runtime->stack);
+    // printf("Result: %llu\n", value);
 }
 
 static void* runMain(void* ctx)
 {
-    printf("Wasm3 v" M3_VERSION " on iOS (" M3_ARCH ")\n");
+    struct utsname systemInfo;
+    uname(&systemInfo);
+
+    printf("Wasm3 v" M3_VERSION " on iOS %s (" M3_ARCH ")\n", systemInfo.machine);
     printf("Build " __DATE__ " " __TIME__ "\n");
-    
-    clock_t start = clock();
+
     run_wasm();
-    clock_t end = clock();
-    
-    printf("Elapsed: %ld ms\n", (end - start)*1000 / CLOCKS_PER_SEC);
-    
     return 0;
 }
 
@@ -89,10 +96,10 @@ static int stdout_redirect(void* prefix, const char* buffer, int size)
 void redirect_output(print_cbk_t f)
 {
     gPrintFunc = f;
-    
+
     setvbuf(stdout, 0, _IOLBF, 0); // stdout: line-buffered
     setvbuf(stderr, 0, _IONBF, 0); // stderr: unbuffered
-    
+
     stdout->_write = stdout_redirect;
     stderr->_write = stdout_redirect;
 }
