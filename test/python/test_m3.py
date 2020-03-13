50af4b868b27b3514bb57449dd82e8f374abdbb2
Alexander Belopolsky <a@enlnt.com>
2020-03-13 00:38:10 -0400

    Add pytest testing

---
+++ test/python/test_m3.py
@@ -0,0 +1,8 @@
+import m3
+
+def test_classes():
+    assert isinstance(m3.Environment, type)
+    assert isinstance(m3.Runtime, type)
+    assert isinstance(m3.Module, type)
+    assert isinstance(m3.Function, type)
+
