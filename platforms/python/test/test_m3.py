b7243377fbadf87d9f0ec01d82c2ad5ed09c29fe
Alexander Belopolsky <a@enlnt.com>
2020-03-13 19:04:26 -0400

    Add a test for fib64'

--- platforms/python/test/test_m3.py
+++ platforms/python/test/test_m3.py
@@ -1,8 +1,19 @@
 import m3
-FIB32_WASM = (b'\x00asm\x01\x00\x00\x00\x01\x06\x01`\x01\x7f\x01\x7f'
-              b'\x03\x02\x01\x00\x07\x07\x01\x03fib\x00\x00\n\x1f\x01'
-              b'\x1d\x00 \x00A\x02I\x04@ \x00\x0f\x0b \x00A\x02k\x10'
-              b'\x00 \x00A\x01k\x10\x00j\x0f\x0b')
+
+FIB32_WASM = bytes.fromhex("""
+ 00 61 73 6d 01 00 00 00 01 06 01 60 01 7f 01 7f
+ 03 02 01 00 07 07 01 03 66 69 62 00 00 0a 1f 01
+ 1d 00 20 00 41 02 49 04 40 20 00 0f 0b 20 00 41
+ 02 6b 10 00 20 00 41 01 6b 10 00 6a 0f 0b
+""")
+
+FIB64_WASM = bytes.fromhex("""
+ 00 61 73 6d 01 00 00 00 01 06 01 60 01 7e 01 7e
+ 03 02 01 00 07 07 01 03 66 69 62 00 00 0a 1f 01
+ 1d 00 20 00 42 02 54 04 40 20 00 0f 0b 20 00 42
+ 02 7d 10 00 20 00 42 01 7d 10 00 7c 0f 0b
+""")
+
 def test_classes():
     assert isinstance(m3.Environment, type)
     assert isinstance(m3.Runtime, type)
@@ -25,3 +36,15 @@ def test_environment():
     assert func.num_args == 1
     assert func.return_type == 1
     assert func.arg_types == (1,)
+
+def call_function(wasm, func, *args):
+    env = m3.Environment()
+    rt = env.new_runtime(1024)
+    mod = env.parse_module(wasm)
+    rt.load(mod)
+    f = rt.find_function(func)
+    return f.call_argv(*args)
+
+def test_fib64():
+    assert call_function(FIB64_WASM, 'fib', '5') == 5
+    assert call_function(FIB64_WASM, 'fib', '10') == 55
