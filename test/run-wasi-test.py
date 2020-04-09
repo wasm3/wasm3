705b5b67645d04e9ca286b2767866a59556d9572
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2020-04-09 22:47:46 +0300

    Add fast (reduced) WASI tests

--- test/run-wasi-test.py
+++ test/run-wasi-test.py
@@ -25,12 +25,13 @@ from pprint import pprint
 parser = argparse.ArgumentParser()
 parser.add_argument("--exec", metavar="<interpreter>", default="../build/wasm3")
 parser.add_argument("--timeout", type=int,             default=120)
+parser.add_argument("--fast",    action='store_true')
 
 args = parser.parse_args()
 
 stats = dotdict(total_run=0, failed=0, crashed=0, timeout=0)
 
-commands = [
+commands_full = [
   {
     "name":           "Simple WASI test",
     "wasm":           "./wasi/test.wasm",
@@ -89,6 +90,42 @@ commands = [
   }
 ]
 
+commands_fast = [
+  {
+    "name":           "Simple WASI test",
+    "wasm":           "./wasi/test.wasm",
+    "args":           ["cat", "./wasi/0.txt"],
+    "expect_pattern": "Hello world*Constructor OK*Args: *; cat; ./wasi/0.txt;*fib(20) = 6765*[* ms]*48 65 6c 6c 6f 20 77 6f 72 6c 64*=== done ===*"
+  }, {
+    "name":           "mandelbrot",
+    "wasm":           "./benchmark/mandelbrot/mandel.wasm",
+    "args":           ["32", "4e5"],
+    "expect_sha1":    "1fdb7dea7ec0f2465054cc623dc5a7225a876361"
+  }, {
+    "name":           "mandelbrot (doubledouble)",
+    "wasm":           "./benchmark/mandelbrot/mandel_dd.wasm",
+    "args":           ["32", "4e5"],
+    "expect_sha1":    "b6d3c158a5c0dff1f6e82a3556c071e4f8b9e3f0"
+  }, {
+    "name":           "C-Ray",
+    "stdin":          "./benchmark/c-ray/scene",
+    "wasm":           "./benchmark/c-ray/c-ray.wasm",
+    "args":           ["-s", "32x32"],
+    "expect_sha1":    "05af9604bf352234276e4d64e84b8d666574316c"
+  }, {
+    "name":           "smallpt (explicit light sampling)",
+    "wasm":           "./benchmark/smallpt/smallpt-ex.wasm",
+    "args":           ["4", "32"],
+    "expect_sha1":    "ea05d85998b2f453b588ef76a1256215bf9b851c"
+  }, {
+    "name":           "Brotli",
+    "stdin":          "./benchmark/brotli/alice29_small.txt",
+    "wasm":           "./benchmark/brotli/brotli.wasm",
+    "args":           ["-c"],
+    "expect_sha1":    "0e8af02a7207c0c617d7d38eed92853c4a619987"
+  }
+]
+
 def fail(msg):
     print(f"{ansi.FAIL}FAIL:{ansi.ENDC} {msg}")
     stats.failed += 1
@@ -97,6 +134,8 @@ def fail(msg):
 args_sep = None
 if "wasmer" in args.exec or "wasmtime" in args.exec:
     args_sep = "--"
+
+commands = commands_fast if args.fast else commands_full
 
 for cmd in commands:
     if "skip" in cmd:
