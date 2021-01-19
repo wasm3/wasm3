be93e88768b39eeed0b77ca1fb215399bda74252
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2021-01-19 21:11:50 +0200

    Move setup.py to python platform

--- platforms/python/setup.py
+++ platforms/python/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from distutils.core import Extension
 from glob import glob
 
-SOURCES = glob('source/*.c') + ['platforms/python/m3module.c']
+SOURCES = glob('m3/*.c') + ['m3module.c']
 
 setup(
     name='wasm3',
     version='0.0.1',
     ext_modules=[
-        Extension('m3', sources=SOURCES, include_dirs=['source'],
+        Extension('m3', sources=SOURCES, include_dirs=['m3'],
         extra_compile_args=['-g', '-O0'])]
 )
