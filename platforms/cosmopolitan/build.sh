e4c571da31e92805192c6c7fdb4bc363a295e3d7
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2021-01-28 09:36:04 +0200

    Enable WASI for cosmopolitan build

--- platforms/cosmopolitan/build.sh
+++ platforms/cosmopolitan/build.sh
@@ -3,7 +3,7 @@ COSMOPOLITAN_URL=https://justine.lol/cosmopolitan/cosmopolitan.zip
 
 SOURCE_DIR=../../source
 
-EXTRA_FLAGS="-Dd_m3PreferStaticAlloc" #-Dd_m3HasWASI
+EXTRA_FLAGS="-Dd_m3PreferStaticAlloc -Dd_m3HasWASI"
 
 STD=./cosmopolitan/std
 
@@ -17,18 +17,21 @@ fi
 if [ ! -d "$STD/sys" ]; then
     # Generate header stubs
     mkdir -p $STD/sys
-    touch $STD/assert.h $STD/limits.h $STD/ctype.h $STD/time.h $STD/errno.h
-    touch $STD/inttypes.h $STD/fcntl.h $STD/math.h $STD/stdarg.h $STD/stdbool.h
-    touch $STD/stdint.h $STD/stdio.h $STD/stdlib.h $STD/string.h $STD/stddef.h
-    touch $STD/sys/types.h $STD/sys/stat.h $STD/unistd.h $STD/sys/uio.h $STD/sys/random.h
+    touch $STD/assert.h $STD/limits.h $STD/ctype.h $STD/time.h $STD/errno.h         \
+          $STD/inttypes.h $STD/fcntl.h $STD/math.h $STD/stdarg.h $STD/stdbool.h     \
+          $STD/stdint.h $STD/stdio.h $STD/stdlib.h $STD/string.h $STD/stddef.h      \
+          $STD/sys/types.h $STD/sys/stat.h $STD/unistd.h $STD/sys/uio.h             \
+          $STD/sys/random.h
 fi
 
 echo "Building Wasm3..."
 
-gcc -g -O -static -fno-pie -no-pie -mno-red-zone -nostdlib -nostdinc           \
-  -Wl,--gc-sections -Wl,-z,max-page-size=0x1000 -fuse-ld=bfd                   \
-  -Wl,-T,cosmopolitan/ape.lds -include cosmopolitan/cosmopolitan.h             \
-  -Wno-format-security -Wfatal-errors $EXTRA_FLAGS                             \
-  -o wasm3.com.dbg -DAPE -I$STD -I$SOURCE_DIR $SOURCE_DIR/*.c ../app/main.c    \
+gcc -g -O -static -fno-pie -no-pie -mno-red-zone -nostdlib -nostdinc                \
+  -Wl,--gc-sections -Wl,-z,max-page-size=0x1000 -fuse-ld=bfd                        \
+  -Wl,-T,cosmopolitan/ape.lds -include cosmopolitan/cosmopolitan.h                  \
+  -Wno-format-security -Wfatal-errors $EXTRA_FLAGS                                  \
+  -o wasm3.com.dbg -DAPE -I$STD -I$SOURCE_DIR $SOURCE_DIR/*.c ../app/main.c         \
   cosmopolitan/crt.o cosmopolitan/ape.o cosmopolitan/cosmopolitan.a
+
 objcopy -SO binary wasm3.com.dbg wasm3.com
+
