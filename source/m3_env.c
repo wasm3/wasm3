2ffd8c62f637e839cd3f85382b920a8d32627a49
Anthony Drendel <me@anthonydrendel.com>
2021-02-20 10:43:43 +0100

    Support exported functions with the same name

--- source/m3_env.c
+++ source/m3_env.c
@@ -41,10 +41,13 @@ void  Function_Release  (IM3Function i_function)
 {
     m3Free (i_function->constants);
 
-    // name can be an alias of fieldUtf8
-    if (i_function->name != i_function->import.fieldUtf8)
+    for (int i = 0; i < i_function->numNames; i++)
     {
-        m3Free (i_function->name);
+        // name can be an alias of fieldUtf8
+        if (i_function->names[i] != i_function->import.fieldUtf8)
+        {
+            m3Free (i_function->names[i]);
+        }
     }
 
     FreeImportInfo (& i_function->import);
@@ -90,10 +93,30 @@ void  Function_FreeCompiledCode (IM3Function i_function)
 
 cstr_t  GetFunctionName  (IM3Function i_function)
 {
-    if (i_function->import.fieldUtf8)
-        return i_function->import.fieldUtf8;
+    u16 numNames = 0;
+    cstr_t *names = GetFunctionNames(i_function, &numNames);
+    if (numNames > 0)
+        return names[0];
     else
-        return (i_function->name) ? i_function->name : "<unnamed>";
+        return "<unnamed>";
+}
+
+
+cstr_t *  GetFunctionNames  (IM3Function i_function, u16 * o_numNames)
+{
+    if (o_numNames == NULL)
+        return NULL;
+
+    if (i_function->import.fieldUtf8)
+    {
+        *o_numNames = 1;
+        return &i_function->import.fieldUtf8;
+    }
+    else
+    {
+        *o_numNames = i_function->numNames;
+        return i_function->names;
+    }
 }
 
 
@@ -692,9 +715,12 @@ void *  v_FindFunction  (IM3Module i_module, const char * const i_name)
 
         bool isImported = f->import.moduleUtf8 or f->import.fieldUtf8;
 
-        if (not isImported and f->name)
+        if (isImported)
+            continue;
+
+        for (int i = 0; i < f->numNames; i++)
         {
-            if (strcmp (f->name, i_name) == 0)
+            if (f->names [i] and strcmp (f->names [i], i_name) == 0)
                 return f;
         }
     }
