4d2c81fa5f6bbd7e843573311a74ac44b6b148d8
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2021-02-24 13:11:33 +0200

    Fix identifier conflict

--- source/m3_api_defs.h
+++ source/m3_api_defs.h
@@ -18,7 +18,7 @@
 #define m3ApiGetArg(TYPE, NAME)    TYPE NAME = * ((TYPE *) (_sp++));
 #define m3ApiGetArgMem(TYPE, NAME) TYPE NAME = (TYPE)m3ApiOffsetToPtr(* ((u32 *) (_sp++)));
 
-#define m3ApiRawFunction(NAME)     const void * NAME (IM3Runtime runtime, IM3ImportContext context, uint64_t * _sp, void * _mem)
+#define m3ApiRawFunction(NAME)     const void * NAME (IM3Runtime runtime, IM3ImportContext _ctx, uint64_t * _sp, void * _mem)
 #define m3ApiReturn(VALUE)         { *raw_return = (VALUE); return m3Err_none; }
 #define m3ApiTrap(VALUE)           { return VALUE; }
 #define m3ApiSuccess()             { return m3Err_none; }
--- source/wasm3.h
+++ source/wasm3.h
@@ -190,7 +190,7 @@ d_m3ErrorConst  (trapStackOverflow,             "[trap] stack overflow")
     M3Result            m3_LoadModule               (IM3Runtime io_runtime,  IM3Module io_module);
     //  LoadModule transfers ownership of a module to the runtime. Do not free modules once successfully imported into the runtime
 
-    typedef const void * (* M3RawCall) (IM3Runtime runtime, IM3ImportContext context, uint64_t * _sp, void * _mem);
+    typedef const void * (* M3RawCall) (IM3Runtime runtime, IM3ImportContext _ctx, uint64_t * _sp, void * _mem);
 
     M3Result            m3_LinkRawFunction          (IM3Module              io_module,
                                                      const char * const     i_moduleName,
