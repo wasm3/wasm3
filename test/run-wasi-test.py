cdee84bd060712226415ed1abcc92bcef150fb8d
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2021-03-02 00:19:01 +0200

    Add MAL (Make a Lisp) wasi test

--- test/run-wasi-test.py
+++ test/run-wasi-test.py
@@ -69,6 +69,11 @@ commands_full = [
     "args":           ["16", "64"],
     "expect_sha1":    "d85df3561eb15f6f0e6f20d5640e8e1306222c6d"
   }, {
+    "name":           "mal",
+    "wasm":           "./benchmark/mal/mal.wasm",
+    "args":           ["./benchmark/mal/test-fib.mal", "16"],
+    "expect_pattern": "987\n",
+  }, {
     "name":           "STREAM",
     "wasm":           "./benchmark/stream/stream.wasm",
     "expect_pattern": "----*Solution Validates:*on all three arrays*----*"
@@ -117,6 +122,11 @@ commands_fast = [
     "wasm":           "./benchmark/smallpt/smallpt-ex.wasm",
     "args":           ["4", "32"],
     "expect_sha1":    "ea05d85998b2f453b588ef76a1256215bf9b851c"
+  }, {
+    "name":           "mal",
+    "wasm":           "./benchmark/mal/mal.wasm",
+    "args":           ["./benchmark/mal/test-fib.mal", "16"],
+    "expect_pattern": "987\n",
   }, {
     "name":           "Brotli",
     "stdin":          "./benchmark/brotli/alice29_small.txt",
