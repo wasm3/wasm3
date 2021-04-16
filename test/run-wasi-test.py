72b0f1b342c77075b26e08996f7fc8ce38d19860
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2021-04-16 10:21:08 +0300

    Enable more Mal tests

--- test/run-wasi-test.py
+++ test/run-wasi-test.py
@@ -65,7 +65,6 @@ commands_full = [
     "args":           ["16", "64"],
     "expect_sha1":    "d85df3561eb15f6f0e6f20d5640e8e1306222c6d"
   }, {
-    "skip":           True,  # Fails on Windows-uvwasi, on CI only (CNR locally)
     "name":           "mal",
     "wasm":           "./wasi/mal/mal.wasm",
     "args":           ["./wasi/mal/test-fib.mal", "16"],
