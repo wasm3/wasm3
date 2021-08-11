f045d11dbf6a4d9b6c6332c6ca60017ed5a6ebc8
Jonathan Hemi <jonathan@hemi.dev>
2021-08-11 15:44:10 +0800

    Allow passing custom UVWASI init options

--- source/m3_api_wasi.h
+++ source/m3_api_wasi.h
@@ -10,6 +10,10 @@
 
 #include "m3_core.h"
 
+#if defined(d_m3HasUVWASI)
+#include "uvwasi.h"
+#endif
+
 d_m3BeginExternC
 
 typedef struct m3_wasi_context_t
@@ -19,7 +23,13 @@ typedef struct m3_wasi_context_t
     ccstr_t *               argv;
 } m3_wasi_context_t;
 
-M3Result    m3_LinkWASI     (IM3Module io_module);
+M3Result    m3_LinkWASI             (IM3Module io_module);
+
+#if defined(d_m3HasUVWASI)
+
+M3Result    m3_LinkWASIWithOptions	(IM3Module io_module, uvwasi_options_t uvwasiOptions);
+
+#endif
 
 m3_wasi_context_t* m3_GetWasiContext();
 
