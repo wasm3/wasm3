f3e179a26c82707df3dfba5a4b652c7b641ea6e3
Philipp Skrupskiy <skrypskii.ph@gmail.com>
2022-08-10 16:40:39 +0300

    minor

--- source/m3_env.c
+++ source/m3_env.c
@@ -836,6 +836,7 @@ M3Result  m3_CallVL  (IM3Function i_function, va_list i_args)
     IM3Runtime runtime = i_function->module->runtime;
     IM3FuncType ftype = i_function->funcType;
     M3Result result = m3Err_none;
+    u8* s = NULL;
 
     if (!i_function->compiled) {
         return m3Err_missingCompiledCode;
@@ -849,7 +850,7 @@ M3Result  m3_CallVL  (IM3Function i_function, va_list i_args)
 
 _   (checkStartFunction(i_function->module))
 
-    u8* s = GetStackPointerForArgs (i_function);
+    s = GetStackPointerForArgs (i_function);
 
     for (u32 i = 0; i < ftype->numArgs; ++i)
     {
@@ -877,6 +878,7 @@ M3Result  m3_Call  (IM3Function i_function, uint32_t i_argc, const void * i_argp
     IM3Runtime runtime = i_function->module->runtime;
     IM3FuncType ftype = i_function->funcType;
     M3Result result = m3Err_none;
+    u8* s = NULL;
 
     if (i_argc != ftype->numArgs) {
         return m3Err_argumentCountMismatch;
@@ -893,7 +895,7 @@ M3Result  m3_Call  (IM3Function i_function, uint32_t i_argc, const void * i_argp
 
 _   (checkStartFunction(i_function->module))
 
-    u8* s = GetStackPointerForArgs (i_function);
+    s = GetStackPointerForArgs (i_function);
 
     for (u32 i = 0; i < ftype->numArgs; ++i)
     {
@@ -921,6 +923,7 @@ M3Result  m3_CallArgv  (IM3Function i_function, uint32_t i_argc, const char * i_
     IM3FuncType ftype = i_function->funcType;
     IM3Runtime runtime = i_function->module->runtime;
     M3Result result = m3Err_none;
+    u8* s = NULL;
 
     if (i_argc != ftype->numArgs) {
         return m3Err_argumentCountMismatch;
@@ -937,7 +940,7 @@ M3Result  m3_CallArgv  (IM3Function i_function, uint32_t i_argc, const char * i_
 
 _   (checkStartFunction(i_function->module))
 
-    u8* s = GetStackPointerForArgs (i_function);
+    s = GetStackPointerForArgs (i_function);
 
     for (u32 i = 0; i < ftype->numArgs; ++i)
     {
