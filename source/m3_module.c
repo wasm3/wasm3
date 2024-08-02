526c1251b64e6e9fdc0d40c768ae46cd20338594
tommie <tommie@users.noreply.github.com>
2024-08-03 00:42:22 +0200

    fix: Releases M3Module.memoryImport. (#490)
    Fixes #462

--- source/m3_module.c
+++ source/m3_module.c
@@ -40,6 +40,7 @@ void  m3_FreeModule  (IM3Module i_module)
             FreeImportInfo(&(i_module->globals[i].import));
         }
         m3_Free (i_module->globals);
+        FreeImportInfo(&i_module->memoryImport);
 
         m3_Free (i_module);
     }
