50577bb5cc86d124a371b00023228cfc59e1f19e
vshymanskyy <vshymanskyi@gmail.com>
2026-08-21 00:21:30 +0300

    Use cosmocc

--- platforms/cosmopolitan/build.sh
+++ platforms/cosmopolitan/build.sh
@@ -1,41 +1,25 @@
+#!/bin/sh
+set -e
 
-COSMOPOLITAN_VERSION=2.2
-COSMOPOLITAN_URL=https://github.com/jart/cosmopolitan/releases/download/$COSMOPOLITAN_VERSION/cosmopolitan-amalgamation-$COSMOPOLITAN_VERSION.zip
+COSMOCC_VERSION=4.0.2
+COSMOCC_URL=https://cosmo.zip/pub/cosmocc/cosmocc-$COSMOCC_VERSION.zip
 
 SOURCE_DIR=../../source
 
-EXTRA_FLAGS="-Dd_m3PreferStaticAlloc -Dd_m3HasWASI"
+EXTRA_FLAGS="-Dd_m3PreferStaticAlloc -Dd_m3HasTypedRefs=1 -Dd_m3HasWASI"
 
-STD=./cosmopolitan/std
-
-if [ ! -d "./cosmopolitan" ]; then
-    echo "Downloading Cosmopolitan..."
-    curl -L -o cosmopolitan.zip $COSMOPOLITAN_URL
-    unzip cosmopolitan.zip -d cosmopolitan
-    rm cosmopolitan.zip
-fi
-
-if [ ! -d "$STD/sys" ]; then
-    # Generate header stubs
-    mkdir -p $STD/sys
-    touch $STD/assert.h $STD/limits.h $STD/ctype.h $STD/time.h $STD/errno.h         \
-          $STD/inttypes.h $STD/fcntl.h $STD/math.h $STD/stdarg.h $STD/stdbool.h     \
-          $STD/stdint.h $STD/stdio.h $STD/stdlib.h $STD/string.h $STD/stddef.h      \
-          $STD/sys/types.h $STD/sys/stat.h $STD/unistd.h $STD/sys/uio.h             \
-          $STD/sys/random.h
+if [ ! -d "./cosmocc" ]; then
+    echo "Downloading Cosmopolitan toolchain..."
+    curl -L -o cosmocc.zip $COSMOCC_URL
+    unzip -q cosmocc.zip -d cosmocc
+    rm cosmocc.zip
+    chmod +x cosmocc/bin/*
 fi
 
 echo "Building Wasm3..."
 
 # TODO: remove -fno-strict-aliasing
 
-gcc -g -Os -static -nostdlib -nostdinc -fno-pie -no-pie -mno-red-zone               \
-  -Wl,--gc-sections -Wl,-z,max-page-size=0x1000 -fuse-ld=bfd                        \
-  -Wl,-T,cosmopolitan/ape.lds -include cosmopolitan/cosmopolitan.h                  \
-  -Wno-format-security -Wfatal-errors -fno-strict-aliasing $EXTRA_FLAGS             \
-  -fomit-frame-pointer -fno-stack-check -fno-stack-protector                        \
-  -o wasm3.com.dbg -DAPE -I$STD -I$SOURCE_DIR $SOURCE_DIR/*.c ../app/main.c         \
-  cosmopolitan/crt.o cosmopolitan/ape.o cosmopolitan/cosmopolitan.a
-
-objcopy -SO binary wasm3.com.dbg wasm3.com
-
+./cosmocc/bin/cosmocc -g -Os -Wfatal-errors -fno-strict-aliasing $EXTRA_FLAGS      \
+  -fomit-frame-pointer -fno-stack-check -fno-stack-protector                       \
+  -o wasm3.com -I$SOURCE_DIR $SOURCE_DIR/*.c ../app/main.c
